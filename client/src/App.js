3c8d39bf9b1edde08610170ba2b10d1bac9e21b4
Nate Magera <nmagera07@gmail.com>
2019-07-19 12:53:00 -0400

    mvp complete

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import Register from './components/Register'
 import ProtectedPage from './components/ProtectedPage'
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
     <div className="App">
       <Router>
+      <div className="nav">
       <Link to="/">Home</Link>
       <Link to="/login">Register</Link>
       <Link to="/protected">Protected</Link>
-      
+      </div>
       <Route path="/login" component={Register} />
       <Route path="/protected" component={ProtectedPage} />
       </Router>
--- client/src/components/ProtectedPage.jsx
+++ client/src/components/ProtectedPage.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
 
 const ProtectedPage = () => {
     const [data, setData] = useState([])
@@ -18,14 +19,18 @@ const ProtectedPage = () => {
     return ( 
         <div>
             <h3>Protected Page</h3>
+            <div className="items">
             {data.map(item => {
-               return <div>
+               return <Card>
+                   {/* <Image src="https://prods3.imgix.net/images/articles/2016_06/Feature-Brisket-Big-Dish-Photo-Devon-Knight.jpg" /> */}
+                    <img src={item.image} alt="food" />
                     <p>Course Name: {item.name}</p>
                     <p>Course Type: {item.course}</p>
                     <p>Technique: {item.technique}</p>
-                    <p>Ingredients: {item.ingredients}</p>
-                </div>
+                    <p>Ingredients: {item.ingredients + ""} </p>
+                </Card>
             })}
+            </div>
         </div>
      );
 }
